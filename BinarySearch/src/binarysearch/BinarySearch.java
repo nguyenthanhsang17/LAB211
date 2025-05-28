2e8ba9d0ef54b4ec6d0452ab8aabc4fa30b15288
nguyenthanhsang17 <nguyenthanhsang17102002@gmail.com>
2025-05-28 11:48:31 +0700

    .

---
+++ BinarySearch/src/binarysearch/BinarySearch.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarysearch;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class BinarySearch {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int numberOfArray = inputValueInt("Enter number of array: ");
+        int [] array = new int[numberOfArray];
+        int value = inputValueInt("Enter search value: ");
+        for (int i = 0; i < numberOfArray; i++) {
+            //array[i] = inputValueInt("arr["+i+"]: ");
+            array[i] = random.nextInt(numberOfArray);
+        }
+        int [] arrayAfterSort = sortArray(array);
+        
+        int start_index=0;
+        int end_index = numberOfArray-1;
+        int find_index = -1;
+        
+        while (start_index<end_index) {  
+            int midle_index = (start_index+end_index)/2;
+            if(arrayAfterSort[midle_index]==value){
+                find_index = midle_index;
+                break;
+            } else
+            if(arrayAfterSort[midle_index]>value){
+                end_index = midle_index-1;
+            }
+            if(arrayAfterSort[midle_index]<value){
+                start_index = midle_index+1;
+            }
+        }
+        
+        for (int i = 0; i < numberOfArray; i++) {
+            System.out.print(arrayAfterSort[i]+",");
+        }
+        
+        if(find_index ==-1){
+            System.out.println("ko tim thay");
+        }else{
+            System.out.println(find_index);
+        }
+        
+    }
+    public static int[] sortArray(int [] array){
+        int length = array.length;
+        for(int i = 0;i<length;i++){
+            for (int j = 0; j < length-1-i; j++) {
+                if(array[j]>array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+    public static String inputValue(String msg) {
+        String input;
+        do {
+            System.out.println(msg);
+            input = sc.nextLine();
+            // check spam spaces loix
+            if (input.trim().equals("")) {
+                System.out.println("Not input spacex");
+                continue;
+            } else {
+                break;
+            }
+        } while (true);
+
+        return input;
+    }
+
+    public static int inputValueInt(String msg) {
+        // "                   "
+        // "vu" => 
+        //"2.3" = 2.3
+        //"12"
+        String input;
+        int result;
+        do {
+            System.out.println(msg);
+            input = sc.nextLine();
+            // check spam spaces loix
+            if (input.trim().equals("")) {
+                System.out.println("not input spacex");
+                continue;
+            }
+            try {
+                double number = Double.parseDouble(input);
+
+                if (number == (int) number) {
+                    result = (int) number;
+                    break;
+                } else {
+                    System.out.println("not input real number");
+                    continue;
+                }
+            } catch (Exception e) {
+                System.out.println("must be input integer number !!!!!!");
+            }
+
+        } while (true);
+        return result;
+    }
+
+}
---
+++ Fibonnaci/src/fibonnaci/Fibonnaci.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonnaci;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author Admin
+ */
+public class Fibonnaci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("The 45 sequence fibonacci");
+        System.out.print("0, 1, ");
+        BigInteger first = new BigInteger("0");
+        BigInteger second = new BigInteger("1");
+        Fibonnaci(first, second, 50);
+    }
+
+    public static int Fibonnaci(BigInteger first, BigInteger second, int number) {
+        if (number == 3) {
+            BigInteger TEMP = first.add(second);
+            System.out.print(TEMP);
+            number--;
+            return Fibonnaci(second, TEMP, number);
+        }
+        if (number > 2) {
+            BigInteger TEMP = first.add(second);
+            System.out.print(TEMP + ", ");
+            number--;
+            return Fibonnaci(second, TEMP, number);
+        } else {
+            return 0;
+        }
+    }
+
+}
